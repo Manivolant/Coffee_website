08136fd32e734ba6a2cec8435e8fa30580e37d39
Manivolant <maninasiri987@gmail.com>
2025-08-11 14:07:21 -0700

    delete loading

--- scripts/script.js
+++ scripts/script.js
@@ -27,11 +27,3 @@ window.addEventListener("scroll",()=>{
 		}
 	}
 });
-
-window.addEventListener("load",()=>{
-	document.getElementById("loading").classList.add("hidden");
-	document.getElementById("loading").classList.add("opacity-0");
-	document.getElementById("loading").classList.remove("opacity-100");
-	document.getElementById("loading").classList.remove("flex");
-	document.getElementById("loading").remove();
-});
